452713117d0e20a5b95ba6925399c9207efe7860
Neil <neil2193@gmail.com>
2020-11-15 15:52:58 +0200

    Updates

---
+++ precache-manifest.183885afd6eb08bad8e60bd0a9c5393c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b829abc41b1db53c282b2cef4b35d8",
+    "url": "/Porto-website/index.html"
+  },
+  {
+    "revision": "886a43f6b4454ea36af8",
+    "url": "/Porto-website/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1def542df3adcc3e1d37",
+    "url": "/Porto-website/static/js/2.ef9b312c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Porto-website/static/js/2.ef9b312c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886a43f6b4454ea36af8",
+    "url": "/Porto-website/static/js/main.dd17adc1.chunk.js"
+  },
+  {
+    "revision": "2ce426b8eccfb5940e37",
+    "url": "/Porto-website/static/js/runtime-main.a65cd8fc.js"
+  }
+]);
